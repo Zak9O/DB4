779166b2fe61d4cc7c9e63f172155df366adde34
linusjuni <s225224@student.dtu.dk>
2024-06-11 10:25:23 +0200

    wifi

---
+++ connection.py
@@ -0,0 +1,29 @@
+import network
+import socket
+import time
+
+def connect_to_wifi(ssid, password):
+    # Turn off the WiFi Access Point
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(False)
+    
+    # Connect the device to the WiFi network
+    wifi = network.WLAN(network.STA_IF)
+    wifi.active(True)
+    wifi.connect(ssid, password)
+    
+    # Wait until the connection is established
+    while not wifi.isconnected():
+        time.sleep(1)
+    
+    print("Connected to WiFi", wifi.ifconfig())
+
+def make_request():
+    request = b"GET / HTTP/1.1\r\nHost: www.dtu.dk\r\n\r\n"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("www.dtu.dk", 80))
+    s.settimeout(2)
+    s.send(request)
+    result = s.recv(10000)
+    print(result)
+    s.close()
---
+++ main.py
@@ -0,0 +1,8 @@
+from connection import make_request, connect_to_wifi
+
+def main():
+    connect_to_wifi("Linus's iPhone (2)", "9ncml4nq4gh9")
+    make_request()
+
+if __name__ == "__main__":
+    main()
